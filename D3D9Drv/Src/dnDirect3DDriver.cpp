7e0e8df2376563f6bc4a33b445c589c27a82b9dd
Justin Marshall <justinmarshall20@gmail.com>
2022-08-31 23:28:41 -0700

    Shadow Map resolution changed from 2048 to 8192.

--- D3D9Drv/Src/dnDirect3DDriver.cpp
+++ D3D9Drv/Src/dnDirect3DDriver.cpp
@@ -11,6 +11,16 @@
 IDirect3D9* g_d3d = nullptr;
 IDirect3DDevice9* g_d3dDevice9 = nullptr;
 
+HRESULT(__stdcall* IDirect3D9_CreateTextureActual)(void* _this, UINT Width, UINT Height, UINT Levels, DWORD Usage, D3DFORMAT Format, D3DPOOL Pool, IDirect3DTexture9** ppTexture, HANDLE* pSharedHandle);
+HRESULT __stdcall IDirect3D9_CreateTexture(void* _this, UINT Width, UINT Height, UINT Levels, DWORD Usage, D3DFORMAT Format, D3DPOOL Pool, IDirect3DTexture9** ppTexture, HANDLE* pSharedHandle) {
+	//if (Width == 2048 && Height == 2048 && Format == D3DFMT_D24S8)
+	//{
+	//	OutputDebugStringW(TEXT("FOUND SHADOW MAP!"));
+	//}
+	
+	return IDirect3D9_CreateTextureActual(_this, Width, Height, Levels, Usage, Format, Pool, ppTexture, pSharedHandle);
+}
+
 HRESULT (__stdcall *IDirect3D9_CreateDeviceActual)(void *_this, UINT Adapter,D3DDEVTYPE DeviceType,HWND hFocusWindow,DWORD BehaviorFlags,D3DPRESENT_PARAMETERS* pPresentationParameters, IDirect3DDevice9** ppReturnedDeviceInterface);
 HRESULT __stdcall IDirect3D9_CreateDevice2(void* _this, UINT Adapter, D3DDEVTYPE DeviceType, HWND hFocusWindow, DWORD BehaviorFlags, D3DPRESENT_PARAMETERS* pPresentationParameters, IDirect3DDevice9** ppReturnedDeviceInterface) {
 	HRESULT hr = IDirect3D9_CreateDeviceActual(_this, Adapter, DeviceType, hFocusWindow, BehaviorFlags, pPresentationParameters, ppReturnedDeviceInterface);
@@ -19,6 +29,14 @@ HRESULT __stdcall IDirect3D9_CreateDevice2(void* _this, UINT Adapter, D3DDEVTYPE
 
 	UEditorEngineVTable* vtable = (UEditorEngineVTable*)g_d3dDevice9;
 	UEditorEngineVTable* vtable_actual = (UEditorEngineVTable*)vtable->vtable[0];
+
+	static bool hookOnce = false;
+	if(!hookOnce)
+	{
+		MH_CreateHook(vtable_actual->vtable[23], IDirect3D9_CreateTexture, (LPVOID*)&IDirect3D9_CreateTextureActual);
+		MH_EnableHook(vtable_actual->vtable[23]);
+		hookOnce = true;
+	}
 
 	return hr;
 }
@@ -31,9 +49,12 @@ IDirect3D9* __stdcall Direct3DCreate9New(UINT SDKVersion) {
 	UEditorEngineVTable* vtable = (UEditorEngineVTable*)g_d3d;
 	UEditorEngineVTable* vtable_actual = (UEditorEngineVTable*)vtable->vtable[0];
 
+	static bool hookOnce = false;
+	if (!hookOnce)
 	{
 		MH_CreateHook(vtable_actual->vtable[16], IDirect3D9_CreateDevice2, (LPVOID*)&IDirect3D9_CreateDeviceActual);
 		MH_EnableHook(vtable_actual->vtable[16]);
+		hookOnce = true;
 	}
 
 	return g_d3d;
